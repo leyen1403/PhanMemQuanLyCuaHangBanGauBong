3e4538abf20c426e0ce2a7c88d1db006fbb03cce
QuanHD0610 <142657399+QuanHD0610@users.noreply.github.com>
2024-11-05 15:32:32 +0700

    tạo form

---
+++ GUI/frm_Main.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frm_main : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+            this.Load += Frm_main1_Load;
+            
+        }
+
+        private void Frm_main1_Load(object sender, EventArgs e)
+        {
+            pnMain.Height = this.ClientSize.Height;
+            pnMain.Width = this.ClientSize.Width - pnLeft.Width;
+            this.MaximizeBox = false;
+            
+        }
+
+        void loadForm(Form form, string titleLabel)
+        {
+            this.Text = form.Text;
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            form.Height = pnMain.Height;
+            form.Width = pnMain.Width;
+            pnMain.Controls.Add(form);
+            pnMain.Tag = form;
+            // Thiết lập tiêu đề của form vào label
+            //label_tenForm.Text = titleLabel;
+            form.BringToFront();
+            form.Show();
+
+        }
+
+        private void btn_LapHoaDon_Click(object sender, EventArgs e)
+        {
+            loadForm(new frm_lapHoaDon(), "");
+        }
+
+        private void btn_LapHoaDon_Click_1(object sender, EventArgs e)
+        {
+            loadForm(new frm_lapHoaDon(), "");
+        }
+    }
+}
---
+++ GUI/frm_lapHoaDon.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GUI
+{
+    partial class frm_lapHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1452, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tiêu đề";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(366, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh sách sản phẩm";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 123);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(911, 538);
+            this.panel1.TabIndex = 2;
+            // 
+            // frm_lapHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1452, 795);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "frm_lapHoaDon";
+            this.Text = " ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
