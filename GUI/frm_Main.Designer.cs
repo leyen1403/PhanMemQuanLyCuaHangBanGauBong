d671d21bf56faef06792f6029f6612249a091014
QuanHD0610 <142657399+QuanHD0610@users.noreply.github.com>
2024-11-07 16:21:09 +0700

    đã sửa chức năng

--- GUI/frm_Main.Designer.cs
+++ GUI/frm_Main.Designer.cs
@@ -42,7 +42,6 @@
             this.btn_LapHoaDon = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.btn_LapPhieuDichVu = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.btn_PhieuNhapHang = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.btn_LapPhieuNhapKho = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.btn_LapThongKeBaoCao = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.btn_LapPhieuKiemKe = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.btn_LapDonDatHang = new DevExpress.XtraBars.Navigation.AccordionControlElement();
@@ -64,6 +63,8 @@
             this.btn_Restore = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_phieuHoanTra = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             ((System.ComponentModel.ISupportInitialize)(this.fluentDesignFormControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fluentFormDefaultManager1)).BeginInit();
             this.pnLeft.SuspendLayout();
@@ -80,9 +81,9 @@
             this.label_tenNV});
             this.fluentDesignFormControl1.Location = new System.Drawing.Point(0, 0);
             this.fluentDesignFormControl1.Manager = this.fluentFormDefaultManager1;
-            this.fluentDesignFormControl1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.fluentDesignFormControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.fluentDesignFormControl1.Name = "fluentDesignFormControl1";
-            this.fluentDesignFormControl1.Size = new System.Drawing.Size(1940, 39);
+            this.fluentDesignFormControl1.Size = new System.Drawing.Size(1661, 31);
             this.fluentDesignFormControl1.TabIndex = 2;
             this.fluentDesignFormControl1.TabStop = false;
             this.fluentDesignFormControl1.TitleItemLinks.Add(this.btn_thietLapTaiKhoan);
@@ -131,10 +132,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnMain.BackColor = System.Drawing.Color.GhostWhite;
             this.pnMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pnMain.Location = new System.Drawing.Point(376, 50);
-            this.pnMain.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.pnMain.Location = new System.Drawing.Point(322, 41);
+            this.pnMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnMain.Name = "pnMain";
-            this.pnMain.Size = new System.Drawing.Size(1606, 1136);
+            this.pnMain.Size = new System.Drawing.Size(1377, 923);
             this.pnMain.TabIndex = 3;
             // 
             // pnLeft
@@ -144,10 +145,10 @@
             this.pnLeft.Controls.Add(this.label1);
             this.pnLeft.Controls.Add(this.pictureBox1);
             this.pnLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnLeft.Location = new System.Drawing.Point(0, 39);
-            this.pnLeft.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.pnLeft.Location = new System.Drawing.Point(0, 31);
+            this.pnLeft.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnLeft.Name = "pnLeft";
-            this.pnLeft.Size = new System.Drawing.Size(373, 1062);
+            this.pnLeft.Size = new System.Drawing.Size(320, 862);
             this.pnLeft.TabIndex = 0;
             this.pnLeft.Paint += new System.Windows.Forms.PaintEventHandler(this.pnLeft_Paint);
             // 
@@ -162,13 +163,13 @@
             this.accordionControlElement1,
             this.accordionControlElement2,
             this.accordionControlElement3});
-            this.accordionControl1.Location = new System.Drawing.Point(5, 78);
-            this.accordionControl1.Margin = new System.Windows.Forms.Padding(10, 2, 10, 2);
+            this.accordionControl1.Location = new System.Drawing.Point(4, 63);
+            this.accordionControl1.Margin = new System.Windows.Forms.Padding(9, 2, 9, 2);
             this.accordionControl1.Name = "accordionControl1";
             this.accordionControl1.OptionsMinimizing.AllowMinimizeMode = DevExpress.Utils.DefaultBoolean.True;
             this.accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Auto;
             this.accordionControl1.ShowFilterControl = DevExpress.XtraBars.Navigation.ShowFilterControl.Auto;
-            this.accordionControl1.Size = new System.Drawing.Size(357, 910);
+            this.accordionControl1.Size = new System.Drawing.Size(306, 739);
             this.accordionControl1.TabIndex = 18;
             // 
             // accordionControlElement1
@@ -177,11 +178,11 @@
             this.btn_LapHoaDon,
             this.btn_LapPhieuDichVu,
             this.btn_PhieuNhapHang,
-            this.btn_LapPhieuNhapKho,
             this.btn_LapThongKeBaoCao,
             this.btn_LapPhieuKiemKe,
             this.btn_LapDonDatHang,
-            this.btn_LapPhieuDoiTra});
+            this.btn_LapPhieuDoiTra,
+            this.btn_phieuHoanTra});
             this.accordionControlElement1.Expanded = true;
             this.accordionControlElement1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement1.ImageOptions.Image")));
             this.accordionControlElement1.Name = "accordionControlElement1";
@@ -208,13 +209,6 @@
             this.btn_PhieuNhapHang.Name = "btn_PhieuNhapHang";
             this.btn_PhieuNhapHang.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
             this.btn_PhieuNhapHang.Text = "Lập phiếu nhập hàng";
-            // 
-            // btn_LapPhieuNhapKho
-            // 
-            this.btn_LapPhieuNhapKho.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_LapPhieuNhapKho.ImageOptions.Image")));
-            this.btn_LapPhieuNhapKho.Name = "btn_LapPhieuNhapKho";
-            this.btn_LapPhieuNhapKho.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.btn_LapPhieuNhapKho.Text = "Lập phiếu nhập kho";
             // 
             // btn_LapThongKeBaoCao
             // 
@@ -248,14 +242,15 @@
             // 
             this.accordionControlElement2.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
             this.btn_HoaDon,
-            this.btn_NhaCC,
+            this.btn_DonDatHang,
+            this.btn_NhapHang,
+            this.accordionControlElement4,
+            this.btn_DoiTra,
             this.btn_DichVu,
+            this.btn_NhaCC,
             this.btn_KhachHang,
             this.btn_Kho,
-            this.btn_DonDatHang,
             this.btn_NhanVien,
-            this.btn_DoiTra,
-            this.btn_NhapHang,
             this.btn_HoanTra,
             this.btn_Loai});
             this.accordionControlElement2.Expanded = true;
@@ -282,7 +277,7 @@
             this.btn_DichVu.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_DichVu.ImageOptions.Image")));
             this.btn_DichVu.Name = "btn_DichVu";
             this.btn_DichVu.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.btn_DichVu.Text = "Quản lý dịch vụ";
+            this.btn_DichVu.Text = "Quản lý phiếu dịch vụ";
             // 
             // btn_KhachHang
             // 
@@ -317,7 +312,7 @@
             this.btn_DoiTra.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_DoiTra.ImageOptions.Image")));
             this.btn_DoiTra.Name = "btn_DoiTra";
             this.btn_DoiTra.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.btn_DoiTra.Text = "Quản lý đổi trả";
+            this.btn_DoiTra.Text = "Quản lý đổi trả sản phẩm";
             // 
             // btn_NhapHang
             // 
@@ -367,10 +362,9 @@
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(2, 70);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(2, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(470, 4);
+            this.label1.Size = new System.Drawing.Size(403, 3);
             this.label1.TabIndex = 17;
             // 
             // pictureBox1
@@ -378,26 +372,40 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = global::GUI.Properties.Resources.logo_gaubong;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(373, 71);
+            this.pictureBox1.Size = new System.Drawing.Size(320, 58);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // btn_phieuHoanTra
+            // 
+            this.btn_phieuHoanTra.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement4.ImageOptions.Image")));
+            this.btn_phieuHoanTra.Name = "btn_phieuHoanTra";
+            this.btn_phieuHoanTra.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.btn_phieuHoanTra.Text = "Lập Phiếu Hoàn Trả NCC";
+            // 
+            // accordionControlElement4
+            // 
+            this.accordionControlElement4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("accordionControlElement4.ImageOptions.Image1")));
+            this.accordionControlElement4.Name = "accordionControlElement4";
+            this.accordionControlElement4.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            this.accordionControlElement4.Text = "Quản lý phiếu kiểm kê";
+            // 
             // frm_main
             // 
             this.Appearance.BackColor = System.Drawing.Color.Navy;
             this.Appearance.Options.UseBackColor = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1940, 1101);
+            this.ClientSize = new System.Drawing.Size(1661, 893);
             this.Controls.Add(this.pnLeft);
             this.Controls.Add(this.pnMain);
             this.Controls.Add(this.fluentDesignFormControl1);
             this.FluentDesignFormControl = this.fluentDesignFormControl1;
-            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frm_main";
             this.Text = "Hệ thống quản lý cửa hàng bán giày";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -425,7 +433,6 @@
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_LapHoaDon;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_LapPhieuDichVu;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_PhieuNhapHang;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement btn_LapPhieuNhapKho;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_LapThongKeBaoCao;
         private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_HoaDon;
@@ -445,5 +452,7 @@
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_NhapHang;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_HoanTra;
         private DevExpress.XtraBars.Navigation.AccordionControlElement btn_Loai;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement btn_phieuHoanTra;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
     }
 }
